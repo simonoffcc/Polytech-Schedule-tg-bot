0a69b38d72bd08a1f8c734150b02082b48c6367b
Ivan Simonenko <simonenkoivansimon@gmail.com>
2024-01-29 00:14:04 +0400

    Added cancel button

--- src/bot/structures/keyboards/to_menu_button.py
+++ src/bot/structures/keyboards/to_menu_button.py
@@ -4,3 +4,8 @@ TO_MENU_BTN = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="На главное меню")],
 ],
     resize_keyboard=True)
+
+CANCEL_BUTTON = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Отмена")],
+],
+    resize_keyboard=True)
