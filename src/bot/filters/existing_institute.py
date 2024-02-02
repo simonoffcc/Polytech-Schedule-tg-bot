a9c7c804b3cc048b1f38a128a2c740de7baa94da
Ivan Simonenko <simonenkoivansimon@gmail.com>
2024-02-03 02:16:07 +0400

    updated todos

--- src/bot/filters/existing_institute.py
+++ src/bot/filters/existing_institute.py
@@ -8,5 +8,3 @@ class IsInstituteExists(BaseFilter):
     async def __call__(self, message: Message) -> bool:
         result: bool = message.text in institutes_acronyms
         return result
-        # todo: Здесь можно добавить проверку на институты типа "ИКНТ"
-        # todo: (в базе расписания и при парсинге он есть, а по факту его нет)
